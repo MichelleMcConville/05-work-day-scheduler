502018fd2980c7ca9cefa1a06395aeebb335e9b8
Michelle McConville <mchelmconvl@gmail.com>
2020-10-11 16:59:08 -0500

    add non working color highlighter

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,30 @@
 //Set date stamp @ top of page
 var today = moment().format("dddd MMMM Do, YYYY");
-console.log(today);
-$("#currentDay").text(today);
+var timeAsOfNow = moment().hour();
+var tasks = [];
 
-  
+//Set current Date in Jumbotron
+$("#currentDay").text(today);
+console.log(today);
+console.log(timeAsOfNow);
+
+//Check time @ set color
+// function timeHighlight() {
+//   $(".hour").each(function () {
+//     console.log(timeAsOfNow);
+
+//     if ($("textarea").attr("id") < timeAsOfNow) {
+//       console.log("I am past time");
+//       $(this).classList.add("past");
+//     } else if ($("textarea").attr("id") > timeAsOfNow) {
+//       console.log("I future time");
+//       $(this).classList.add("future");
+//     } else {
+//       console.log("I am present time");
+//       $(this).classList.add("present");
+//     }
+//   });
+// }
+// timeHighlight();
+
 
