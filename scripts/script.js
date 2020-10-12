0ff8b5f5a4874f3912e90e469bde0ef5d052db14
Michelle McConville <mchelmconvl@gmail.com>
2020-10-12 11:04:10 -0500

    added clear all calendar button

--- scripts/script.js
+++ scripts/script.js
@@ -66,76 +66,81 @@ h18 = parseInt(h18[0]);
 //Saving ToDo list to local storage on click f(x)
 //Row for 7:00 AM
 $(".b07").on("click", function () {
-    var lsContent = $(".t07").val();
-    localStorage.setItem("07:00", lsContent);
-  });
-  //Row for 8:00 AM
-  $(".b08").on("click", function () {
-    var lsContent = $(".t08").val();
-    localStorage.setItem("08:00", lsContent);
-  });
-  //Row for 9:00 AM
-  $(".b09").on("click", function () {
-    var lsContent = $(".t09").val();
-    localStorage.setItem("09:00", lsContent);
-  });
-  //Row for 10:00 AM
-  $(".b10").on("click", function () {
-    var lsContent = $(".t10").val();
-    localStorage.setItem("10:00", lsContent);
-  });
-  //Row for 11:00 AM
-  $(".b11").on("click", function () {
-    var lsContent = $(".t11").val();
-    localStorage.setItem("11:00", lsContent);
-  });
-  //Row for 12:00 PM
-  $(".b12").on("click", function () {
-    var lsContent = $(".t12").val();
-    localStorage.setItem("12:00", lsContent);
-  });
-  //Row for 1:00 PM
-  $(".b13").on("click", function () {
-    var lsContent = $(".t13").val();
-    localStorage.setItem("13:00", lsContent);
-  });
-  //Row for 2:00 PM
-  $(".b14").on("click", function () {
-    var lsContent = $(".t14").val();
-    localStorage.setItem("14:00", lsContent);
-  });
-  //Row for 3:00 PM
-  $(".b15").on("click", function () {
-    var lsContent = $(".t15").val();
-    localStorage.setItem("15:00", lsContent);
-  });
-  //Row for 4:00 PM
-  $(".b16").on("click", function () {
-    var lsContent = $(".t16").val();
-    localStorage.setItem("16:00", lsContent);
-  });
-  //Row for 5:00 PM
-  $(".b17").on("click", function () {
-    var lsContent = $(".t17").val();
-    localStorage.setItem("17:00", lsContent);
-  });
-  //Row for 6:00 PM
-  $(".b18").on("click", function () {
-    var lsContent = $(".t18").val();
-    localStorage.setItem("18:00", lsContent);
-  });
-  
-  // Getting ToDo list from local storage
-  $(".t07").val(localStorage.getItem("07:00"));
-  $(".t08").val(localStorage.getItem("08:00"));
-  $(".t09").val(localStorage.getItem("09:00"));
-  $(".t10").val(localStorage.getItem("10:00"));
-  $(".t11").val(localStorage.getItem("11:00"));
-  $(".t12").val(localStorage.getItem("12:00"));
-  $(".t13").val(localStorage.getItem("13:00"));
-  $(".t14").val(localStorage.getItem("14:00"));
-  $(".t15").val(localStorage.getItem("15:00"));
-  $(".t16").val(localStorage.getItem("16:00"));
-  $(".t17").val(localStorage.getItem("17:00"));
-  $(".t18").val(localStorage.getItem("18:00"));
-  
+  var lsContent = $(".t07").val();
+  localStorage.setItem("07:00", lsContent);
+});
+//Row for 8:00 AM
+$(".b08").on("click", function () {
+  var lsContent = $(".t08").val();
+  localStorage.setItem("08:00", lsContent);
+});
+//Row for 9:00 AM
+$(".b09").on("click", function () {
+  var lsContent = $(".t09").val();
+  localStorage.setItem("09:00", lsContent);
+});
+//Row for 10:00 AM
+$(".b10").on("click", function () {
+  var lsContent = $(".t10").val();
+  localStorage.setItem("10:00", lsContent);
+});
+//Row for 11:00 AM
+$(".b11").on("click", function () {
+  var lsContent = $(".t11").val();
+  localStorage.setItem("11:00", lsContent);
+});
+//Row for 12:00 PM
+$(".b12").on("click", function () {
+  var lsContent = $(".t12").val();
+  localStorage.setItem("12:00", lsContent);
+});
+//Row for 1:00 PM
+$(".b13").on("click", function () {
+  var lsContent = $(".t13").val();
+  localStorage.setItem("13:00", lsContent);
+});
+//Row for 2:00 PM
+$(".b14").on("click", function () {
+  var lsContent = $(".t14").val();
+  localStorage.setItem("14:00", lsContent);
+});
+//Row for 3:00 PM
+$(".b15").on("click", function () {
+  var lsContent = $(".t15").val();
+  localStorage.setItem("15:00", lsContent);
+});
+//Row for 4:00 PM
+$(".b16").on("click", function () {
+  var lsContent = $(".t16").val();
+  localStorage.setItem("16:00", lsContent);
+});
+//Row for 5:00 PM
+$(".b17").on("click", function () {
+  var lsContent = $(".t17").val();
+  localStorage.setItem("17:00", lsContent);
+});
+//Row for 6:00 PM
+$(".b18").on("click", function () {
+  var lsContent = $(".t18").val();
+  localStorage.setItem("18:00", lsContent);
+});
+
+// Getting ToDo list from local storage
+$(".t07").val(localStorage.getItem("07:00"));
+$(".t08").val(localStorage.getItem("08:00"));
+$(".t09").val(localStorage.getItem("09:00"));
+$(".t10").val(localStorage.getItem("10:00"));
+$(".t11").val(localStorage.getItem("11:00"));
+$(".t12").val(localStorage.getItem("12:00"));
+$(".t13").val(localStorage.getItem("13:00"));
+$(".t14").val(localStorage.getItem("14:00"));
+$(".t15").val(localStorage.getItem("15:00"));
+$(".t16").val(localStorage.getItem("16:00"));
+$(".t17").val(localStorage.getItem("17:00"));
+$(".t18").val(localStorage.getItem("18:00"));
+
+//Clearing all calendar button
+$(".clearBtn").on("click", function () {
+  localStorage.clear();
+  location.reload();
+});
