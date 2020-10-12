ccbc13cdf99d79130f8daaec9836e8b422e1774a
Michelle McConville <mchelmconvl@gmail.com>
2020-10-12 10:54:07 -0500

    adding local storage function

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,35 @@
-// Variables List
 var today = moment().format("dddd MMMM Do, YYYY");
 var timeAsOfNow = moment().hour();
-var hours = $("textarea");
-var toDo = [];
-var textBox;
+var hour = [h10];
+var toDo = [t10];
 
-//Set current Date in Jumbotron
+//Setting current Date in Jumbotron
 $("#currentDay").text(today);
 console.log(today);
 console.log(timeAsOfNow);
 
-//Check time @ set color
+//Checking time @ setting color
 // function timeHighlight() {
 //   $(".time-block").each(function () {
-//     console.log(timeAsOfNow);
-
-//     if ($(this).attr("id") < timeAsOfNow) {
-//       console.log("I am past time");
-//       $(this).("past");
-//     }
-//     if ($(this).attr("id") === timeAsOfNow) {
-//       console.log("I am present time");
-//       $(this).addClass("present");
-//     }
-//     if ($(this).attr("id") > timeAsOfNow) {
-//       console.log("I am future time");
-//       $(this).addClass("future");
-//     }
+//     for (var i = 0; i < hour.length; i++) {
+//       if (hour[i] < timeAsOfNow) {
+//         toDo[i].classList.add("past");
+//       } else if (hour[i] === timeAsOfNow) {
+//         toDo[i].classList.add("present");
+//       } else (hour[i] > timeAsOfNow)
+//         toDo[i].classList.add("future");
+//       }
+//     })
 //   }
-// )};
-
 // timeHighlight();
 
+var h10 = document.querySelector(".h10").textContent;
+h10 = parseInt(h10[0]);
+var t10 = document.querySelector(".t10");
 
+//Row for 10:00 AM
+$(".b10").on("click", function () {
+    var lsTime = $(".t10").val();
+    localStorage.setItem("10:00", lsTime);
+});
+$(".t10").val(localStorage.getItem("10:00"));
